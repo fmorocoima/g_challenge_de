7f665c99b4c5140241c833b6edcc07eca8403042
Freddy Morocoima <morocoimafredd@gmail.com>
2023-10-24 02:04:58 -0300

    env test

--- g_challenge_de/src/settings.py
+++ g_challenge_de/src/settings.py
@@ -13,7 +13,7 @@ if not os.getenv('ENVIRONMENT', False):
 # Server settings:
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'prod')
 HOST = '0.0.0.0'
-PORT = int(os.getenv('PORT', 8010))
+PORT = int(os.getenv('PORT', 8020))
 
 
 # Database settings:
